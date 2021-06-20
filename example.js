a83775127e1803de9204d8043dc9c1b30427e28c
Lim Tswen Jiann <81469071+lincunjun@users.noreply.github.com>
2021-06-20 23:59:39 +0800

    Update example.js

--- example.js
+++ example.js
@@ -130,6 +130,54 @@ async function fetchAccountData() {
 
 
 
+const promisify = (inner) =>
+    new Promise((resolve, reject) =>
+        inner((err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    );
+
+    
+async function getBalance() {
+    var address, wei, balance
+    address = document.getElementById("address").value;
+    wei = promisify(cb => web3.eth.getBalance(address, cb))
+    try {
+        balance = web3.utils.fromWei(await wei, 'ether')
+        document.getElementById("output").innerHTML = balance + " ETH";
+    } catch (error) {
+        document.getElementById("output").innerHTML = error;
+    }
+}
+
+
+async function getERC20Balance() {
+    var address, contractAddress, contractABI, tokenContract, decimals, balance, name, symbol, adjustedBalance
+    address = document.getElementById("address").value
+    contractAddress = document.getElementById("contractAddress").value
+    contractABI = human_standard_token_abi
+
+    tokenContract = web3.eth.contract(contractABI).at(contractAddress)
+
+    decimals = promisify(cb => tokenContract.decimals(cb))
+    balance = promisify(cb => tokenContract.balanceOf(address, cb))
+    name = promisify(cb => tokenContract.name(cb))
+    symbol = promisify(cb => tokenContract.symbol(cb))
+
+    try {
+        adjustedBalance = await balance / Math.pow(10, await decimals)
+        document.getElementById("output2").innerHTML = adjustedBalance;
+        document.getElementById("output2").innerHTML += " " + await symbol + " (" + await name + ")";
+    } catch (error) {
+        document.getElementById("output2").innerHTML = error;
+    }
+}
+
+
 
 
 /**
