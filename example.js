91f659c88b773f9f0fdcfc2cd52255433f3723fd
Lim Tswen Jiann <81469071+lincunjun@users.noreply.github.com>
2021-06-21 14:36:20 +0800

    Update example.js

--- example.js
+++ example.js
@@ -1,12 +1,3 @@
-window.addEventListener('load', function () {
-  if (typeof web3 !== 'undefined') {
-      console.log('Web3 Detected! ' + web3.currentProvider.constructor.name)
-      window.web3 = new Web3(web3.currentProvider);
-  } else {
-      console.log('No Web3 Detected... using HTTP Provider')
-      window.web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/<APIKEY>"));
-  }
-})
 
 "use strict";
 
@@ -30,7 +21,7 @@ let provider;
 // Address of the selected account
 let selectedAccount;
 
-console.log(web3.version);
+console.log("web version is " , web3.version);
 /**
  * Setup the orchestra
  */
