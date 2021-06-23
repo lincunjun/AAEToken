58a9a2e18d0dfe52e52448b3f43e44379ec4d7be
Lim Tswen Jiann <81469071+lincunjun@users.noreply.github.com>
2021-06-23 12:00:43 +0800

    Update example.js

--- example.js
+++ example.js
@@ -179,10 +179,10 @@ async function getERC20Balance() {
 
     tokenContract = new web3.eth.Contract(contractABI , contractAddress)
 
-    decimals = promisify(cb => tokenContract.methods.decimals());
-    balance = promisify(cb => tokenContract.methods.balanceOf(address));
-    name = promisify(cb => tokenContract.methods.name());
-    symbol = promisify(cb => tokenContract.methods.symbol());
+    decimals = promisify(cb() => tokenContract.methods.decimals(cb));
+    balance = promisify(cb() => tokenContract.methods.balanceOf(address, cb));
+    name = promisify(cb() => tokenContract.methods.name(cb));
+    symbol = promisify(cb() => tokenContract.methods.symbol(cb));
   console.log(decimals);
   console.log(balance);
   console.log(name);
